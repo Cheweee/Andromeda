b894ff8407e8c5e19668b74388d1765de61e4328
Кирилл <kni2012.15@gmail.com>
2018-05-15 02:16:01 +0300

    Created role and rights pages

--- Web/Controllers/AdministrationController.cs
+++ Web/Controllers/AdministrationController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Andromeda.Core.Managers;
+using Andromeda.Models.Administration;
+using Andromeda.ViewModels.Client;
+using Andromeda.ViewModels.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,15 +17,81 @@ namespace Andromeda.Web.Controllers
         {
             return View();
         }
+        #region Roles
         [HttpGet]
         public ActionResult Roles()
         {
             return View();
         }
+
+        [HttpGet]
+        public JsonResult GetRoles(EntitiesViewModel model)
+        {
+            var data = BaseEntityManager.GetEntities<Role, RoleViewModel>(
+                model.Page,
+                model.Limit,
+                model.Order,
+                model.IsAscending(),
+                o=> o.Name.ToLower().Contains((model.Search ?? string.Empty).ToLower()),
+                o=> new RoleViewModel { Id = o.Id, Name = o.Name });
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public JsonResult GetRoleRights(EntitiesViewModel model)
+        {
+            var data = BaseEntityManager.GetEntities<Right, RightViewModel>(
+                o=> o.Name.ToLower().Contains((model.Search ?? string.Empty).ToLower()),
+                o=> new RightViewModel { Id = o.Id, Name = o.Name });
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult AddRole(Role entity)
+        {
+            entity.Id = Guid.NewGuid();
+            var data = BaseEntityManager.AddEntity(entity);
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult ModifyRole(Role entity)
+        {
+            var data = BaseEntityManager.ModifyEntity(entity);
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult DeleteRoles(List<Role> entities)
+        {
+            var data = BaseEntityManager.DeleteEntities(entities);
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        #endregion
+        #region Rights
         [HttpGet]
         public ActionResult Rights()
         {
             return View();
         }
+        [HttpGet]
+        public JsonResult GetRights(EntitiesViewModel model)
+        {
+            var data = BaseEntityManager.GetEntities<Right, RightViewModel>(
+                model.Page,
+                model.Limit,
+                model.Order,
+                model.IsAscending(),
+                o => o.Name.ToLower().Contains((model.Search ?? string.Empty).ToLower()),
+                o => new RightViewModel
+                {
+                    Id = o.Id,
+                    Name = o.Name
+                });
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        #endregion
     }
 }
---
+++ Web/Scripts/Andromeda/Controllers/andromeda.controller.role.js
@@ -0,0 +1,117 @@
+﻿app.controller('roleController', function ($scope, $q, $timeout, $mdDialog, $filter, service) {
+    var result = false;
+    var createTimer = function () {
+        $timeout(function () {
+            // loading
+            $scope.loading = false;
+        }, 500);
+    };
+
+    $scope.closeDialog = function () {
+        $mdDialog.hide();
+    };
+
+    $scope.rights = [];
+    $scope.searchRole = '';
+    $scope.selectedRole = null;
+
+    $scope.getFaculties = function () {
+        var deferred = $q.defer();
+        service.getEntities('/Administration/GetRoleRights'
+            , model = { Search: $scope.searchRole }).then(function (response) {
+                if (response.data.Result) {
+                    var rows = response.data.Entities;
+                    $scope.rights = rows;
+                    deferred.resolve(rows);
+                }
+            });
+
+        return deferred.promise;
+    };
+
+    $scope.entity = {
+        Id: null,
+        Name: '',
+        ShortName: '',
+        FacultyName: '',
+        FacultyId: null,
+        Code: 0
+    };
+
+    $scope.settings = {
+        delay: 500,
+        loading: false,
+        message: 'Загрузка данных',
+        addition: false,
+        addOrEdit: service.addOrEdit,
+        closeDialog: function () {
+            service.closeDialog();
+        }
+    };
+
+    $scope.loadDialog = function () {
+        $scope.message = 'Загрузка данных';
+        if (!$scope.settings.addOrEdit) {
+            $scope.loading = true;
+            service.getEntityById('/References/GetDepartment', service.id)
+                .then(function (args) {
+                    if (args.data.Result) {
+                        $scope.entity.Id = args.data.Entity.Id;
+                        $scope.entity.Code = args.data.Entity.Code;
+                        $scope.entity.Name = args.data.Entity.Name;
+                        $scope.entity.ShortName = args.data.Entity.ShortName;
+                        $scope.entity.FacultyId = args.data.Entity.FacultyId;
+                        $scope.getFaculties().then(function () {
+                            var rows = $scope.rights;
+                            for (i = 0; i < rows.length; i++) {
+                                if ($scope.entity !== null) {
+                                    if ($scope.entity.FacultyId === rows[i].Id) {
+                                        $scope.selectedRole = rows[i];
+                                    }
+                                }
+                            }
+                            createTimer();
+                        });
+                    }
+                });
+        }
+    };
+
+    $scope.clearFields = function () {
+        if (!$scope.settings.addOrEdit) {
+            $scope.loadDialog();
+            return;
+        }
+        $scope.entity.Name = '';
+        $scope.entity.ShortName = '';
+        $scope.entity.FacultyName = '';
+        $scope.entity.FacultyId = null;
+        $scope.selectedRole = null;
+        $scope.entity.Code = 0;
+    };
+
+    $scope.confirm = function () {
+        var facultyId = null;
+        if ($scope.selectedRole) {
+            facultyId = $scope.selectedRole.Id;
+        }
+        var entity = {
+            Id: $scope.entity.Id,
+            Name: $scope.entity.Name,
+            ShortName: $scope.entity.ShortName,
+            Code: $scope.entity.Code,
+            FacultyId: facultyId
+        };
+
+        $scope.message = 'Сохранение изменений';
+        $scope.loading = true;
+        result = true;
+        var method = service.addOrEdit ? "/References/AddDepartment" : "/References/ModifyDepartment";
+        service.addOrEditEntity(method, entity)
+            .then(function (response) {
+                if (response.data.Result) {
+                    $scope.closeDialog();
+                }
+            });
+    };
+});
