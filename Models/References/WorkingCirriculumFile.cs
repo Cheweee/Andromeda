f3f225f978fb516ffc2c49726cdd6594222e0d32
Кирилл <kni2012.15@gmail.com>
2018-05-21 20:04:29 +0300

    test 2

---
+++ Models/References/WorkingCirriculumFile.cs
@@ -0,0 +1,16 @@
+﻿using Andromeda.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andromeda.Models.References
+{
+    public class WorkingCirriculumFile : IKeyEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public Guid WorkingCirriculumId { get; set; }
+        public byte[] FileData { get; set; }
+    }
+}
